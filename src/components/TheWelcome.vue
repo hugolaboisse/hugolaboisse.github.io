7ff4844504e2821aa9e82b8ddf227796573d71ac
Hugo <hllaboisse@dons.usfca.edu>
2022-02-07 20:37:49 -0800

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,50 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Books</template>
+    He reads manga.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Schooling</template>
+    He is a student at USF.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Volume</template>
+    He takes up 65.35 liters of space.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Socialization</template>
+    He chats on his phone.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>All Time Favorite Frontend Framework</template>
+    Vue!
+  </WelcomeItem>
+</template>
